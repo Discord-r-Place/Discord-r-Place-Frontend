1cd035b012e7cbd6b1ce894754d7d91df5d24e7e
J.Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-11-12 18:43:20 +0100

    Dropped body margin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          body {
+            margin: 0px;
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
